8d6c45eacbd47cf3bd3a14adc906fe4384453801
Andrei <andrewshubaderov@gmail.com>
2026-01-31 20:03:05 +0700

    feat: add fastapi-users configuration

---
+++ app/core/auth.py
@@ -0,0 +1,36 @@
+import uuid
+from fastapi_users import FastAPIUsers
+from fastapi_users.authentication import JWTStrategy, AuthenticationBackend
+
+from app.db.models.user import User
+from app.core.config import settings
+from app.core.user_manager import UserManager
+from app.db.user_db import get_user_db
+from fastapi import Depends
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(
+        secret=settings.secret_key,
+        lifetime_seconds=3600,
+    )
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=None,
+    get_strategy=get_jwt_strategy,
+)
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
+
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend],
+)
+
+current_user = fastapi_users.current_user()
+current_active_user = fastapi_users.current_user(active=True)
