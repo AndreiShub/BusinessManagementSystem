121054178e2627584cfc15dde3d0d293e8edda19
Andrei <andrewshubaderov@gmail.com>
2026-02-05 22:01:26 +0700

    feat: add admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,51 @@
 from sqladmin import Admin, ModelView
 from fastapi import FastAPI
 from app.db.session import engine
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
 from app.db.models import User, Team, Task, TeamMember
+from app.core.auth import get_user_manager_direct
+from typing import Callable, AsyncGenerator
+from fastapi_users.exceptions import UserNotExists
+
+
+class FastAPIUsersAdminAuth(AuthenticationBackend):
+    def __init__(self, get_user_manager: Callable[[], AsyncGenerator]):
+        self.get_user_manager = get_user_manager
+        self.middlewares = []
+
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        email = form.get("username")
+        password = form.get("password")
+
+        try:
+            user_manager = await anext(self.get_user_manager())
+            user = await user_manager.get_by_email(email)
+        except UserNotExists:
+            return False
+
+        if user.is_superuser and await user_manager.verify_password(
+            password, user.hashed_password
+        ):
+            return True
+
+        return False
+
+    async def logout(self, request: Request) -> None:
+        pass
+
+    async def authenticate(self, request: Request) -> bool:
+        return False
 
 
 def setup_admin(app: FastAPI):
-    admin = Admin(app, engine, title="My Admin Panel")
+    admin = Admin(
+        app,
+        engine,
+        title="My Admin Panel",
+        authentication_backend=FastAPIUsersAdminAuth(get_user_manager_direct),
+    )
 
     class UserAdmin(ModelView, model=User):
         pass
@@ -15,7 +55,7 @@ def setup_admin(app: FastAPI):
 
     class TaskAdmin(ModelView, model=Task):
         pass
-    
+
     class TeamMemberAdmin(ModelView, model=TeamMember):
         pass
 
